4d7b413d55dfb27c819ef484f6fc44f7cbb9984e
Er4ld0 <junior-eraldo2011@hotmail.com>
2024-09-18 22:48:51 -0300

    atualizacao do contador de itens do carrinho

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,25 +14,19 @@ function checkRestaurantOpen() {
 export default function Footer() {
 
   const [modalIsOpen, setModalIsOpen] = useState(false);
-    const [isOpen, setIsOpen] = useState(checkRestaurantOpen());
+  const [isOpen, setIsOpen] = useState(checkRestaurantOpen());
+  const [cartItemCount, setCartItemCount] = useState(Cart.length);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
       setIsOpen(checkRestaurantOpen());
+      setCartItemCount(Cart.length);
     }, 60000);
 
     return () => clearInterval(intervalId);
   }, []);
 
-  const CartItemCounter = (cart: any[]) => {
-    const calculateTotalQuantity = (cart: any[]) => {
-      return cart.reduce((total: any, item: { quantity: any; }) => total + item.quantity, 0);
-    };
 
-    const totalQuantity = calculateTotalQuantity(cart);
-
-    return <span>{totalQuantity}</span>;
-  };
 
   function handleOpenModal() {
     setModalIsOpen(!modalIsOpen);
@@ -40,21 +34,21 @@ export default function Footer() {
 
   return (
 
-      <footer className="w-full h-11 bg-[#BF0404] hover:bg-[#cf3434] fixed bottom-0 z-40 flex justify-center rounded-t-2xl">
-        {/* BOTÃO DE ABRIR O MODAL */}
-        <button
-          className="w-full flex items-center justify-center gap-2 text-white font-bold"
-          id="cart-btn"
-          onClick={handleOpenModal}
-        >
-          {/* CONTADOR DE ITENS DO CARRINHO */}
-          <span id="cart-count">({CartItemCounter(Cart)})</span>
-          Meu carinho
-          {/* ÍCONE DO BOTÃO DE ABRIR O MODAL */}
-          <FaCartPlus className="text-2xl py-1" />
-        </button>
-        <Modal isOpen={modalIsOpen} onClose={handleOpenModal} />
-      </footer>
-      
-      )
+    <footer className="w-full h-11 bg-[#BF0404] hover:bg-[#cf3434] fixed bottom-0 z-40 flex justify-center rounded-t-2xl">
+      {/* BOTÃO DE ABRIR O MODAL */}
+      <button
+        className="w-full flex items-center justify-center gap-2 text-white font-bold"
+        id="cart-btn"
+        onClick={handleOpenModal}
+      >
+        {/* CONTADOR DE ITENS DO CARRINHO */}
+        <span id="cart-count">({cartItemCount})</span>
+        Meu carinho
+        {/* ÍCONE DO BOTÃO DE ABRIR O MODAL */}
+        <FaCartPlus className="text-2xl py-1" />
+      </button>
+      <Modal isOpen={modalIsOpen} onClose={handleOpenModal} />
+    </footer>
+
+  )
 }
